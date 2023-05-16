1e2aa7fcb7323006c6129c70714a526417ca8dd6
mickb <mickbreijer@outlook.com>
2023-05-16 14:54:35 +0200

    setup

--- src/js/resources.js
+++ src/js/resources.js
@@ -4,6 +4,9 @@ import fishImage from '../images/fish.png'
 const Resources = {
     Fish: new ImageSource(fishImage)
 }
-const ResourceLoader = new Loader([Resources.Fish])
+const ResourceLoader = new Loader([
+    Resources.Fish,
+
+])
 
 export { Resources, ResourceLoader }
